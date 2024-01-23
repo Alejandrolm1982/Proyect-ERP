8abd80a1f3b69ce681c6557e3aacc3a06091c28c
Alejandro <118361930+Alejandrolm1982@users.noreply.github.com>
2024-01-23 13:02:16 +0100

    clientesproveedor primer interfaz funcional

--- Assets/js/motor.js
+++ Assets/js/motor.js
@@ -1,19 +1,6 @@
-function deshabilitarControl1(control1)
-{
-    control1.classList.remove("boton1");
-    control1.classList.add("boton1Inhabilitado");
-    control1.disable = true;
-}
-
-function habilitarControl1(control1)
-{
-    control1.classList.remove("boton1Inhabilitado");
-    control1.classList.add("boton1");
-    control1.disable = false;
-}
 
 /* INICIO - ajaxPost1 - Devuelve el resultado a un DIV (POST) */
-function ajaxPost1(controlador1, div1)
+function ajaxPost1(valorSeleccionado, controlador1, div1)
 {
 
   // 1.- Creación del objeto XMLHttpRequest (Ajax1)
@@ -31,209 +18,52 @@ function ajaxPost1(controlador1, div1)
   Ajax1.open("POST", controlador1);
   
   // 4.-Envío del formulario a través del FormData
-  Ajax1.send();
+  Ajax1.send(valorSeleccionado);
 }
 /* FIN - ajaxPost1 - Devuelve el resultado a un DIV (POST) */
 
-/* INICIO - ajaxPost2 - Devuelve el resultado a una Alert (POST) */
-function ajaxPost2(form1, controlador1, div1)
-{
+function seleccionarDatos1(valorSeleccionado, controlador1, div1) {
+  // Creación del objeto FormData y agregando el valor seleccionado
+  const formData = new FormData();
+  formData.append("cliente/proveedor", valorSeleccionado);
 
-  // 1.- Creación del objeto XMLHttpRequest (Ajax1)
-  const Ajax1 = new XMLHttpRequest();
+  // Imprime el contenido del FormData en la consola para verificar
+  console.log("Contenido de FormData:", formData);
 
-  // 2.- Enlace del formulario a un objeto FormData (FormData1)
-  const FormData1 = new FormData( form1 );
-  
-  // 5.-Éxito en el envío
-  Ajax1.addEventListener("load", function(event) {
-    alert(this.responseText);
-  });
-  
-  // 5.-Error en el envío
-  Ajax1.addEventListener("error", function( event ) {
-    alert( 'Error: no se ha enviado la informacion' );
-  } );
-  
-  // 3.-Configuración del envío del formulario a través del FormData
-  Ajax1.open("POST", controlador1);
-  
-  // 4.-Envío del formulario a través del FormData
-  Ajax1.send( FormData1 );
-  
-}
-/* FIN - ajaxPost2 - Devuelve el resultado a una Alert (POST) */
-
-/* INICIO - ajaxGet1 - Devuelve el resultado a un DIV (GET) */
-function ajaxGet1(controlador1, div1) {
- 
-  let ajax1 = new XMLHttpRequest();
-  ajax1.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200)
-    {
-      document.getElementById(div1.id).innerHTML = this.responseText;
-    }
-  };
-  ajax1.open("GET", controlador1, true);
-  ajax1.send();
-
-}
-/* FIN - ajaxGet1 - Devuelve el resultado a un DIV (GET) */
-
-function seleccionarDatos1(controlador1, div1)
-{    
-    ajaxPost1(controlador1,div1);
+  // Llamada a la función ajaxPost1 con el objeto FormData
+  ajaxPost1(formData, controlador1, div1);
 }
 
-function seleccionarDatos2(form1, boton1, controlador1, div1)
-{
-  deshabilitarControl1(boton1);
-  ajaxGet1(controlador1, div1);
-  habilitarControl1(boton1);
-  form1.reset();
-}
-
-function seleccionarDatos3(form1, boton1, controlador1, div1)
-{
-  deshabilitarControl1(boton1);
-  ajaxGet1(controlador1+'?valor=' + campo1.value, div1);
-  //ajaxGet1('Controllers/Buscador1Controller.php?valor=' + campo1.value, divResultado1.id);
-  habilitarControl1(boton1);
-  form1.reset();
-}
-
-function insertarDatos1(form1, boton1, controlador1, div1)
-{      
-  deshabilitarControl1(boton1);
-  // Opcion 1: El mensaje se muestra en un div (ajaxPost1)
-  ajaxPost1(form1,controlador1,div1);
-  // Opcion 2: El mensaje se muestra en una alert (ajaxPost2)
-  // ajaxPost2(form1,controlador1,div1);
-  habilitarControl1(boton1);
-  form1.reset();
-}
 
 window.addEventListener('load', function(){
 
-  let boton1;
-  let boton2;
   let controlador1;
-  let controlador2;
   let div1;
-  let div2;
 
+/* ---------------------------------- INICIO - (submit) Seleccionar 1 */
+// Paso 1: Obtener referencias:
+const selectClienteProveedor = document.getElementById("cliente/proveedor");
+// Paso 2 - Asociación del elemento al evento (change) y llamada a la función
+if (selectClienteProveedor) {
+  // Referencia de los elementos
+  controlador1 = "Controllers/ClienteProveedor1Controller.php";
+  div1 = document.getElementById("container1");
 
+  selectClienteProveedor.addEventListener("change", function(event) {
+    event.preventDefault();
 
+    // Obtén el valor seleccionado del <select>
+    const valorSeleccionado = selectClienteProveedor.value;
 
-  /* ---------------------------------- INICIO - (submit) Seleccionar 1 */
-  // Paso 1: Obtener referencias:
-  const selectClienteProveedor = document.getElementById("cliente/proveedor");
-  // Paso 2 - Asociación del elemento al evento (submit) y llamada a la función
-  if(selectClienteProveedor.value == "todos")
-  {
-    // Referencia de los elementos
-    controlador1 = "Controllers/ClienteProveedor1Controller.php";
-    div1 = document.getElementById("container1");
-    seleccionarDatos1(controlador1,div1);
-  }
-  /* ---------------------------------- FIN - (submit) Seleccionar 1 */
-
-
-
-
-
-  
-
-  /* ---------------------------------- INICIO - (click) Seleccionar 2 */
-  // Paso 1 - Referencia del elemento que tiene asociado el evento
-  const botonConsulta2 = document.getElementById("botonConsulta2");
-  // Paso 2 - Asociación del elemento al evento y llamada a la función
-  if(botonConsulta2)
-  {
-    // Referencia de los elementos
-    controlador2 = "Controllers/Consulta2Controller.php";
-    div2 = document.getElementById("contenedor2");
-    // Evento y llamada a la función
-    botonConsulta2.addEventListener("click", function(event){
-      event.preventDefault();
-      seleccionarDatos2(formConsulta1,botonConsulta2,controlador2,div2);
-    });
-  }
-  /* ---------------------------------- FIN - (click) Seleccionar 2 */
-
-  /* ---------------------------------- INICIO - (submit) Insertar 1 */
-  // Paso 1: Obtener referencias:
-  const formInsercion1 = document.getElementById("formInsercion1");
-  // Paso 2 - Asociación del elemento al evento (submit) y llamada a la función
-  if(formInsercion1)
-  {
-    // Referencia de los elementos
-    boton1 = document.getElementById("botonInsercion1");
-    controlador1 = "Controllers/Insercion1Controller.php";
-    div1 = document.getElementById("contenedor2");
-    // Evento y llamada a la función
-    formInsercion1.addEventListener("submit", function(event){
-      event.preventDefault();
-      insertarDatos1(formInsercion1,boton1,controlador1,div1);
-    });
-  }
-  /* ---------------------------------- FIN - (submit) Insertar 1 */
-
-  /* ---------------------------------- INICIO - (submit) Insertar 2 */
-  // Paso 1: Obtener referencias:
-  const formInsercion2 = document.getElementById("formInsercion2");
-  // Paso 2 - Asociación del elemento al evento (submit) y llamada a la función
-  if(formInsercion2)
-  {
-    // Referencia de los elementos
-    boton1 = document.getElementById("botonInsercion2");
-    controlador1 = "Controllers/InsercionConsulta1Controller.php";
-    div1 = document.getElementById("contenedor2");
-    // Evento y llamada a la función
-    formInsercion2.addEventListener("submit", function(event){
-      event.preventDefault();
-      insertarDatos1(formInsercion2,boton1,controlador1,div1);
-    });
-  }
-  /* ---------------------------------- FIN - (submit) Insertar 2 */
-
-    /* ---------------------------------- INICIO - (submit) Insertar 3 */
-  // Paso 1: Obtener referencias:
-  const formInsercion3 = document.getElementById("formInsercion3");
-  // Paso 2 - Asociación del elemento al evento (submit) y llamada a la función
-  if(formInsercion3)
-  {
-    // Referencia de los elementos
-    boton1 = document.getElementById("botonInsercion3");
-    controlador1 = "Controllers/InsercionDeclaracionesPreparadas1Controller.php";
-    div1 = document.getElementById("contenedor2");
-    // Evento y llamada a la función
-    formInsercion3.addEventListener("submit", function(event){
-      event.preventDefault();
-      insertarDatos1(formInsercion3,boton1,controlador1,div1);
-    });
-  }
-  /* ---------------------------------- FIN - (submit) Insertar 3 */
-
-  /* ---------------------------------- INICIO - (submit) Insertar y subir archivos 1 */
-  // Paso 1: Obtener referencias:
-  const formSubidaArchivos1 = document.getElementById("formSubidaArchivos1");
-  // Paso 2 - Asociación del elemento al evento (submit) y llamada a la función
-  if(formSubidaArchivos1)
-  {
-    // Referencia de los elementos
-    boton1 = document.getElementById("botonInsercion1");
-    controlador1 = "Controllers/InsercionConSubidaDeArchivos1Controller.php";
-    div1 = document.getElementById("contenedor2");
-    // Evento y llamada a la función
-    formSubidaArchivos1.addEventListener("submit", function(event){
-      event.preventDefault();
-      insertarDatos1(formSubidaArchivos1,boton1,controlador1,div1);
-    });
-  }
-  /* ---------------------------------- FIN - (submit) Insertar y subir archivos 1  */
+    // Llama a la función pasando el valor seleccionado
+    seleccionarDatos1(valorSeleccionado, controlador1, div1);
+  });
+}
+/* ---------------------------------- FIN - (submit) Seleccionar 1 */
 
 });
+
+
 
 //Login JS
 let switchCtn = document.querySelector("#switch-cnt");
--- Controllers/ClienteProveedor1Controller.php
+++ Controllers/ClienteProveedor1Controller.php
@@ -1,63 +1,74 @@
 <?php
   
-  if(file_exists("../Db/Con1Db.php"))
-  {
-      // Llamada a la conexión
-      require_once '../Db/Con1Db.php';
-      //Llamada al modelo
-      require_once '../Models/ClienteProveedor1Model.php';
+if (file_exists("../Db/Con1Db.php")) {
+    // Llamada a la conexión
+    require_once '../Db/Con1Db.php';
+    // Llamada al modelo
+    require_once '../Models/ClienteProveedor1Model.php';
+} else if (file_exists("Db/Con1Db.php")) {
+    // Llamada a la conexión
+    require_once 'Db/Con1Db.php';
+    // Llamada al modelo
+    require_once 'Models/ClienteProveedor1Model.php';
+}
 
-  }
-  else if(file_exists("Db/Con1Db.php"))
-  {
-       // Llamada a la conexión
-      require_once 'Db/Con1Db.php';
-      //Llamada al modelo
-      require_once 'Models/ClienteProveedor1Model.php';
+// Instancia del objeto
+$oData = new Datos;
 
-  }
+// Obtén el tipo seleccionado del formulario
+$selectTipo = empty($_POST['cliente/proveedor']) ? '' : $_POST['cliente/proveedor'];
 
-    // Instancia del objeto
-    $oData = new Datos;
+// Inicializa $sql para evitar el warning de undefined variable
+$sql = "";
 
-    // Llamada al método
-    $sql = "select * from clienteproveedor";
+// Construye la consulta SQL según el tipo seleccionado
+if ($selectTipo == 'todos') 
+{
+    $sql = "SELECT * FROM clienteproveedor";
+} 
+elseif ($selectTipo == 'cliente') 
+{
+    $sql = "SELECT * FROM clienteproveedor WHERE tipo = 'cliente'";
+} 
+elseif ($selectTipo == 'proveedor') 
+{
+    $sql = "SELECT * FROM clienteproveedor WHERE tipo = 'proveedor'";
+}
+
+// Verifica que $sql no esté vacío antes de usarlo en getData1
+if (!empty($sql)) {
     $data = $oData->getData1($sql);
 
-    if(empty($data))
-    {
-        echo
-        "
+    if (empty($data)) {
+        echo "
             <div class='bloque1 negrita'>
                 No hay datos.
             </div>
         ";
-    }
-    else
-    {
-        echo
-        "
-        <div class='bloque0 negrita'>
-            <div class='bloque1'>NOMBRE</div>
-            <div class='bloque1'>DIRECCION</div>
-            <div class='bloque1'>TELÉFONO</div>
-            <div class='bloque1'>CORREO</div>
-            <div class='bloque1'>TIPO</div>
-        </div>
-        ";
-        foreach ($data as $row)
-        {
-            echo
-            "
-            <div class='bloque0'>
-                <div class='bloque1'>$row->nombre</div>
-                <div class='bloque1'>$row->direccion</div>
-                <div class='bloque1'>$row->telefono</div>
-                <div class='bloque1'>$row->correo</div>
-                <div class='bloque1'>$row->tipo</div>
+    } else {
+        echo "
+            <div class='bloque0 negrita'>
+                <div class='bloque1'>NOMBRE</div>
+                <div class='bloque1'>DIRECCION</div>
+                <div class='bloque1'>TELÉFONO</div>
+                <div class='bloque1'>CORREO</div>
+                <div class='bloque1'>TIPO</div>
             </div>
+        ";
+        foreach ($data as $row) {
+            echo "
+                <div class='bloque0'>
+                    <div class='bloque1'>$row->nombre</div>
+                    <div class='bloque1'>$row->direccion</div>
+                    <div class='bloque1'>$row->telefono</div>
+                    <div class='bloque1'>$row->correo</div>
+                    <div class='bloque1'>$row->tipo</div>
+                </div>
             ";
         }
     }
+} else {
+    echo "Consulta SQL vacía.";
+}
 
 ?>
